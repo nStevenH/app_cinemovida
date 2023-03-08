8b292c104dcd2472002dbbb705d6876814e55efb
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-08 21:25:13 +0100

    Different screens of the BottomNavBar

---
+++ lib/features/movies/presentation/pages/cinema/screen/cinema_screen.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+
+
+
+class LocalisationScreen extends StatelessWidget {
+  const LocalisationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: const [
+            Text('Mon cinéma...')
+          ],
+        ),
+      ),
+      );
+  }
+}
