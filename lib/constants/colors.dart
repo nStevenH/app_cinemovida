d9f20b8f567a28eda8a9dabf2e96e3e529197773
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-08 20:16:23 +0100

    Adding color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color blue = Color(0xFF020935);
+const Color yellow = Color(0xFFF8AE1B);
+const Color grey = Color(0xFF626262);
+const Color transparent = Colors.transparent;
+
+const Color testPrimary = black;
+const Color textSecondary = white;
+const Color testTertiary = grey;
+const Color icon = white;
+const Color background = Color(0xFFF6F6F6);
+const Color card = Color(0xFFE6E6E6);
