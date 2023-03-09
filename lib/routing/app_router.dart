f5cdbe2600de712639c27d206d23f3e09ee132ae
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-09 01:36:24 +0100

    Added shell routes for the use of the NavBar

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-
 import 'package:app_cinemovida/routing/scaffold_with_bottom_nav_bar.dart';
 import 'package:app_cinemovida/src/features/movies/presentation/pages/cinema/screen/cinema_screen.dart';
 import 'package:app_cinemovida/src/features/movies/presentation/pages/coming-soon/screen/coming-soon_screen.dart';
 import 'package:app_cinemovida/src/features/movies/presentation/pages/display/screen/display_screen.dart';
 import 'package:app_cinemovida/src/features/movies/presentation/pages/events/screen/events_screen.dart';
 import 'package:app_cinemovida/src/features/movies/presentation/pages/tickets/tickets_details/tickets_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 
 enum AppRoute {
@@ -16,62 +15,71 @@ enum AppRoute {
   events,
   tickets,
   cinema,
-  setting,
 }
 
 // private navigators
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
-final _sheetNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
-    initialLocation:'/onDisplay',
+    initialLocation: '/onDisplay',
     navigatorKey: _rootNavigatorKey,
     debugLogDiagnostics: true,
     routes: [
-        ShellRoute(
-          navigatorKey: _sheetNavigatorKey,
-          builder: (context, state, child) {
-            return ScaffoldBottomNavBar(child: child);
-          },
-          routes: [
-            GoRoute(
-              path: 'onDisplay',
-              name: AppRoute.onDisplay.name,
-              pageBuilder: (context, state) => const NoTransitionPage(
-                  child: OnDisplayScreen(),
-              )
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) {
+          return ScaffoldBottomNavBar(child: child);
+        },
+        routes: [
+          // A l'affiche
+          GoRoute(
+            path: '/onDisplay',
+            name: AppRoute.onDisplay.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const OnDisplayScreen(),
             ),
-            GoRoute(
-                path: 'comingSoon',
-                name: AppRoute.comingSoon.name,
-                pageBuilder: (context, state) => const NoTransitionPage(
-                  child: ComingSoonScreen(),
-                )
+          ),
+          // A venir
+          GoRoute(
+            path: '/comingSoon',
+            name: AppRoute.comingSoon.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const ComingSoonScreen(),
             ),
-            GoRoute(
-                path: 'events',
-                name: AppRoute.events.name,
-                pageBuilder: (context, state) => const NoTransitionPage(
-                  child: EventsScreen(),
-                )
+          ),
+          // Evenements
+          GoRoute(
+            path: '/events',
+            name: AppRoute.events.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const EventsScreen(),
             ),
-            GoRoute(
-                path: 'tickets',
-                name: AppRoute.tickets.name,
-                pageBuilder: (context, state) => const NoTransitionPage(
-                  child: TicketsScreen(),
-                )
-            ),
-            GoRoute(
-                path: 'cinema',
-                name: AppRoute.cinema.name,
-                pageBuilder: (context, state) => const NoTransitionPage(
-                  child: LocalisationScreen(),
-                )
-            ),
-          ],
-        ),
-      ],
+          ),
+          // Mes billets
+          GoRoute(
+              path: '/tickets',
+              name: AppRoute.tickets.name,
+              pageBuilder: (context, state) => NoTransitionPage(
+                key: state.pageKey,
+                child: const TicketsScreen(),
+              ),
+          ),
+          // Mon cinema
+          GoRoute(
+              path: '/cinema',
+              name: AppRoute.cinema.name,
+              pageBuilder: (context, state) => NoTransitionPage(
+                key: state.pageKey,
+                child: const LocalisationScreen(),
+              ),
+          ),
+        ],
+      ),
+    ],
   );
 });
