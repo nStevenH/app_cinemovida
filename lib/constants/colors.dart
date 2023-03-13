40e9f5419216b10e360bc2fbfe64f1044b6efdbc
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-13 17:30:23 +0100

    Correction of the color variable textTertiary

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -9,7 +9,7 @@ const Color transparent = Colors.transparent;
 
 const Color testPrimary = black;
 const Color textSecondary = white;
-const Color testTertiary = grey;
+const Color textTertiary = grey;
 const Color icon = white;
 const Color background = Color(0xFFF6F6F6);
 const Color card = Color(0xFFE6E6E6);
