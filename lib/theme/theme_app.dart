28f0b2d3f762a68eaa2eac88cd05ace5ef0ec91e
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-13 17:27:03 +0100

    Full customization of the application theme. AppBar - BottomNavBar - Button

---
+++ lib/theme/theme_app.dart
@@ -0,0 +1,39 @@
+import 'package:app_cinemovida/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    appBarTheme: const AppBarTheme(
+      backgroundColor: blue,
+      elevation: 0,
+      centerTitle: true,
+
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: blue,
+      type: BottomNavigationBarType.fixed,
+      unselectedIconTheme: const IconThemeData(color: icon),
+      unselectedItemColor: textTertiary,
+      unselectedLabelStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 10.sp,
+        fontFamily: 'roboto',
+        fontWeight: FontWeight.w500,
+      ),
+      selectedItemColor: yellow,
+      selectedLabelStyle: TextStyle(
+        color: textSecondary,
+        fontSize: 10.sp,
+        fontFamily: 'roboto',
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: blue,
+      elevation: 4,
+    ),
+    fontFamily: 'Roboto',
+    scaffoldBackgroundColor: background,
+  );
+}
