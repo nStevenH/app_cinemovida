4e83b8b6833297d19d0400a9f292448eeef7ec8d
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-14 13:32:27 +0100

    update sizes

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
+
 class Sizes {
-  static const p4 = 4.0;
-  static const p8 = 8.0;
-  static const p12 = 12.0;
-  static const p16 = 16.0;
-  static const p20 = 20.0;
-  static const p24 = 24.0;
-  static const p32 = 32.0;
-  static const p48 = 48.0;
-  static const p64 = 64.0;
+  static double p4 = 4.0.sp;
+  static double p8 = 8.0.sp;
+  static double p12 = 12.0.sp;
+  static double p16 = 16.0.sp;
+  static double p20 = 20.0.sp;
+  static double p24 = 24.0.sp;
+  static double p32 = 32.0.sp;
+  static double p48 = 48.0.sp;
+  static double p64 = 64.0.sp;
+}
+class SizesH {
+  static double p4 = 4.0.h;
+  static double p8 = 8.0.h;
+  static double p12 = 12.0.h;
+  static double p16 = 16.0.h;
+  static double p20 = 20.0.h;
+  static double p24 = 24.0.h;
+  static double p32 = 32.0.h;
+  static double p48 = 48.0.h;
+  static double p64 = 64.0.h;
+}
+
+class SizesW {
+  static double p4 = 4.0.w;
+  static double p8 = 8.0.w;
+  static double p12 = 12.0.w;
+  static double p16 = 16.0.w;
+  static double p20 = 20.0.w;
+  static double p24 = 24.0.w;
+  static double p32 = 32.0.w;
+  static double p48 = 48.0.w;
+  static double p64 = 64.0.w;
 }
 
 /// Constant gap widths
-const gapW4 = SizedBox(width: Sizes.p4);
-const gapW8 = SizedBox(width: Sizes.p8);
-const gapW12 = SizedBox(width: Sizes.p12);
-const gapW16 = SizedBox(width: Sizes.p16);
-const gapW20 = SizedBox(width: Sizes.p20);
-const gapW24 = SizedBox(width: Sizes.p24);
-const gapW32 = SizedBox(width: Sizes.p32);
-const gapW48 = SizedBox(width: Sizes.p48);
-const gapW64 = SizedBox(width: Sizes.p64);
+SizedBox gapW4 = SizedBox(width: SizesW.p4);
+SizedBox gapW8 = SizedBox(width: SizesW.p8);
+SizedBox gapW12 = SizedBox(width: SizesW.p12);
+SizedBox gapW16 = SizedBox(width: SizesW.p16);
+SizedBox gapW20 = SizedBox(width: SizesW.p20);
+SizedBox gapW24 = SizedBox(width: SizesW.p24);
+SizedBox gapW32 = SizedBox(width: SizesW.p32);
+SizedBox gapW48 = SizedBox(width: SizesW.p48);
+SizedBox gapW64 = SizedBox(width: SizesW.p64);
 
 /// Constant gap heights
-const gapH4 = SizedBox(height: Sizes.p4);
-const gapH8 = SizedBox(height: Sizes.p8);
-const gapH12 = SizedBox(height: Sizes.p12);
-const gapH16 = SizedBox(height: Sizes.p16);
-const gapH20 = SizedBox(height: Sizes.p20);
-const gapH24 = SizedBox(height: Sizes.p24);
-const gapH32 = SizedBox(height: Sizes.p32);
-const gapH48 = SizedBox(height: Sizes.p48);
-const gapH64 = SizedBox(height: Sizes.p64);
+SizedBox gapH4 = SizedBox(height: SizesH.p4);
+SizedBox gapH8 = SizedBox(height: SizesH.p8);
+SizedBox gapH12 = SizedBox(height: SizesH.p12);
+SizedBox gapH16 = SizedBox(height: SizesH.p16);
+SizedBox gapH20 = SizedBox(height: SizesH.p20);
+SizedBox gapH24 = SizedBox(height: SizesH.p24);
+SizedBox gapH32 = SizedBox(height: SizesH.p32);
+SizedBox gapH48 = SizedBox(height: SizesH.p48);
+SizedBox gapH64 = SizedBox(height: SizesH.p64);
