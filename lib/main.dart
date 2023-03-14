1e30e58b21c29b05e45363bd19e5d2b40d3b2db0
nStevenH <steven.huguenin@gmail.com>
2023-03-15 00:12:30 +0100

    update import firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:app_cinemovida/src/cinemovida_app.dart';
 
-import 'firebase_options.dart';
+import 'package:app_cinemovida/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
