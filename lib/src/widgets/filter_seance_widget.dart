7c4a8ab39509ba7a9d85c2e7ce87eb8bad8c4dbd
nStevenH <steven.huguenin@gmail.com>
2023-03-15 00:37:08 +0100

    Custom widget to filter sessions

---
+++ lib/src/widgets/filter_seance_widget.dart
@@ -0,0 +1,34 @@
+import 'package:app_cinemovida/src/constants/colors.dart';
+import 'package:app_cinemovida/src/constants/sizes.dart';
+import 'package:app_cinemovida/src/constants/style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FilterSeanceWidget extends StatelessWidget {
+  const FilterSeanceWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        color: yellow,
+        height: 30.h,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.filter_list_outlined,
+              color: icon,
+            ),
+            gapW8,
+            Text(
+              'FILTRER LES SÉANCES',
+              style: textTitleWC,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
