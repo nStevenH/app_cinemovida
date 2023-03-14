db1f9170fed6fae3f04ce9391b2ecf5f0a33c7bd
nStevenH <steven.huguenin@gmail.com>
2023-03-15 00:11:50 +0100

    Adding style typography

---
+++ lib/src/constants/style.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import 'package:app_cinemovida/src/constants/colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// text title (size 16 - fontWeight 600 - letterSpacing -0.15)
+var textTitleB = TextStyle(
+    color: black,
+    fontSize: 16.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.15
+);
+var textTitleW = TextStyle(
+  color: white,
+  fontSize: 16.sp,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.15,
+);
+var textTitleG = TextStyle(
+    color: grey,
+    fontSize: 16.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.15
+);
+// text title filter seance (size 16 - fontWeight 600 - letterSpacing -0.35)
+var textTitleWC = TextStyle(
+    color: white,
+    fontSize: 16.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.35
+);
+// text subtitle (size 14 - fontWeight 500)
+var textSubtitleB = TextStyle(
+    color: black,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500
+);
+var textSubtitleW = TextStyle(
+    color: white,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500
+);
+var textSubtitleG = TextStyle(
+    color: grey,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500
+);
+// text body (size 14 - fontWeight 400)
+var textBodyB = TextStyle(
+    color: black,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
+var textBodyW = TextStyle(
+    color: white,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
+var textBodyG = TextStyle(
+    color: grey,
+    fontSize: 14.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
+// text caption (size 12 - fontWeight 400)
+var textCaptionB = TextStyle(
+    color: black,
+    fontSize: 12.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
+var textCaptionW = TextStyle(
+    color: white,
+    fontSize: 12.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
+var textCaptionG = TextStyle(
+    color: grey,
+    fontSize: 12.sp,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400
+);
