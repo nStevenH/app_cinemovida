4bc06be00b40549da9e6c5f24e0da3c57d6ddcee
nStevenH <steven.huguenin@gmail.com>
2023-03-17 22:39:57 +0100

    Reste à effectuer la génération d'item + les actions de boutons qui correspondront à chaque film + faire le routeur de changement de page du bouton floatingbutton

--- lib/src/features/pages/movies/presentation/screen/display_screen.dart
+++ lib/src/features/pages/movies/presentation/screen/display_screen.dart
@@ -1,7 +1,9 @@
 import 'package:app_cinemovida/src/common/widgets/choice_cinema_widget.dart';
 import 'package:app_cinemovida/src/common/widgets/filter_seance_widget.dart';
 import 'package:app_cinemovida/src/common/widgets/list_day_widget.dart';
+import 'package:app_cinemovida/src/common/widgets/movie_info_event_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class OnDisplayScreen extends StatelessWidget {
   const OnDisplayScreen({Key? key}) : super(key: key);
@@ -9,17 +11,24 @@ class OnDisplayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: const [
           ChoiceCinemaWidget(),
           ListDayWidget(),
           FilterSeanceWidget(),
+          MovieInfoEventWidget(),
+          MovieInfoEventWidget(),
+          MovieInfoEventWidget(),
+          MovieInfoEventWidget(),
+          MovieInfoEventWidget(),
         ],
       ),
       floatingActionButton: FloatingActionButton(
 
         child: const Icon(Icons.read_more_outlined),
-        onPressed: () {},
+        onPressed: () {
+          context.goNamed('/realtimeMovie');
+        },
       ),
     );
   }
