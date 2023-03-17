69ca05adbcae7615dcd3fe2a24a4d38533f8791e
nStevenH <steven.huguenin@gmail.com>
2023-03-17 22:37:24 +0100

    Reste à effectuer la génération d'item + les actions de boutons qui correspondront à chaque film.

--- lib/src/features/pages/coming-soon/presentation/screen/coming-soon_screen.dart
+++ lib/src/features/pages/coming-soon/presentation/screen/coming-soon_screen.dart
@@ -1,9 +1,11 @@
+import 'package:app_cinemovida/src/common/components/container_reusable.dart';
+import 'package:app_cinemovida/src/common/widgets/choice_cinema_widget.dart';
+import 'package:app_cinemovida/src/common/widgets/movie_event_widget.dart';
+import 'package:app_cinemovida/src/common/widgets/movie_info_event_widget.dart';
+import 'package:app_cinemovida/src/constants/colors.dart';
+import 'package:app_cinemovida/src/constants/style.dart';
 import 'package:flutter/material.dart';
-
-
-
-
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ComingSoonScreen extends StatelessWidget {
   const ComingSoonScreen({Key? key}) : super(key: key);
@@ -11,10 +13,53 @@ class ComingSoonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: const [
-            Text('A venir...')
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: ListView(
+          children: [
+            const ChoiceCinemaWidget(),
+            ReusableContainer(
+              color: yellow,
+              height: 28.w,
+              padding: 8.sp,
+              width: double.infinity,
+              child: Text('LES PLUS ATTENDUS', style: textTitleWC),
+            ),
+            const EventMovieWidget(),
+            ReusableContainer(
+              color: const Color(0xFFEAEAEA),
+              height: 20.w,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    '22 Mars 2022',
+                    style: textBodyB,
+                  ),
+                ),
+              ),
+            ),
+            const MovieInfoEventWidget(),
+            ReusableContainer(
+              color: const Color(0xFFEAEAEA),
+              height: 20.w,
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    '23 Mars 2022',
+                    style: textBodyB,
+                  ),
+                ),
+              ),
+            ),
+            const MovieInfoEventWidget(),
+            const MovieInfoEventWidget(),
+            const MovieInfoEventWidget(),
           ],
         ),
       ),
