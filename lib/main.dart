90f766d2e61027cd75148ced08d2648fc608612c
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-18 12:42:37 +0100

    update import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:app_cinemovida/src/cinemovida_app.dart';
-
-import 'package:app_cinemovida/firebase_options.dart';
+import 'package:app_cinemovida/src/utils/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,8 +10,6 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-      const ProviderScope(
-          child: MyApp()
-      ),
+    const ProviderScope(child: MyApp()),
   );
 }
