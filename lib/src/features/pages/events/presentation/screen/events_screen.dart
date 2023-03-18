9d28a9cf65e4af15458103274ede7ef4abcac005
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-18 21:25:32 +0100

    Faire la génération d'item + les redirection page sur les informations de chaque evenements

--- lib/src/features/pages/events/presentation/screen/events_screen.dart
+++ lib/src/features/pages/events/presentation/screen/events_screen.dart
@@ -1,6 +1,8 @@
+import 'package:app_cinemovida/src/features/pages/events/application/widget/event_list_widget.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:app_cinemovida/src/common/widgets/choice_cinema_widget.dart';
+import 'package:app_cinemovida/src/constants/colors.dart';
+import 'package:app_cinemovida/src/constants/sizes.dart';
 
 
 class EventsScreen extends StatelessWidget {
@@ -9,12 +11,33 @@ class EventsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: const [
-            Text('Evenements')
-          ],
-        ),
+      backgroundColor: card,
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              gapH56,
+              const EventListWidget(textTitle: 'PRINTEMPS DU CINEMA',),
+              gapH8,
+              const EventListWidget(textTitle: 'CINEPOUSSETTE',),
+              gapH8,
+              const EventListWidget(textTitle: 'FILM EN VOST',),
+              gapH8,
+              const EventListWidget(textTitle: 'SME',),
+              gapH8,
+              const EventListWidget(textTitle: 'ICE IMMERSIVE',),
+              gapH8,
+              const EventListWidget(textTitle: 'MX4D',),
+              gapH8,
+              const EventListWidget(textTitle: 'FILM COUP DE COEUR',),
+              gapH8,
+              const EventListWidget(textTitle: 'MET OPERA',),
+              gapH8,
+              const EventListWidget(textTitle: 'AVANT-PREMIERE',),
+            ],
+          ),
+          const ChoiceCinemaWidget(),
+        ],
       ),
     );
   }
