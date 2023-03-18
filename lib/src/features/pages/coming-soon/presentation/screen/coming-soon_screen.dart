35777ef4121d7e232db8bf1855d999c0d1f90f63
Steven Huguenin <steven.huguenin@gmail.com>
2023-03-18 21:22:07 +0100

    ChoiceCinema fixed

--- lib/src/features/pages/coming-soon/presentation/screen/coming-soon_screen.dart
+++ lib/src/features/pages/coming-soon/presentation/screen/coming-soon_screen.dart
@@ -3,6 +3,7 @@ import 'package:app_cinemovida/src/common/widgets/choice_cinema_widget.dart';
 import 'package:app_cinemovida/src/common/widgets/movie_event_widget.dart';
 import 'package:app_cinemovida/src/common/widgets/movie_info_event_widget.dart';
 import 'package:app_cinemovida/src/constants/colors.dart';
+import 'package:app_cinemovida/src/constants/sizes.dart';
 import 'package:app_cinemovida/src/constants/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,55 +14,92 @@ class ComingSoonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.topLeft,
-        child: ListView(
-          children: [
-            const ChoiceCinemaWidget(),
-            ReusableContainer(
-              color: yellow,
-              height: 28.w,
-              padding: 8.sp,
-              width: double.infinity,
-              child: Text('LES PLUS ATTENDUS', style: textTitleWC),
-            ),
-            const EventMovieWidget(),
-            ReusableContainer(
-              color: const Color(0xFFEAEAEA),
-              height: 20.w,
-              width: double.infinity,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    '22 Mars 2022',
-                    style: textBodyB,
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              gapH32,
+              ReusableContainer(
+                color: yellow,
+                height: 28.w,
+                padding: 8.sp,
+                width: double.infinity,
+                child: Text('LES PLUS ATTENDUS', style: textTitleWC),
+              ),
+              const EventMovieWidget(),
+              ReusableContainer(
+                color: const Color(0xFFEAEAEA),
+                height: 20.w,
+                width: double.infinity,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      '22 Mars 2022',
+                      style: textBodyB,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const MovieInfoEventWidget(),
-            ReusableContainer(
-              color: const Color(0xFFEAEAEA),
-              height: 20.w,
-              width: double.infinity,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    '23 Mars 2022',
-                    style: textBodyB,
+              const MovieInfoEventWidget(),
+              ReusableContainer(
+                color: const Color(0xFFEAEAEA),
+                height: 20.w,
+                width: double.infinity,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      '23 Mars 2023',
+                      style: textBodyB,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const MovieInfoEventWidget(),
-            const MovieInfoEventWidget(),
-            const MovieInfoEventWidget(),
-          ],
-        ),
+              const MovieInfoEventWidget(),
+              const MovieInfoEventWidget(),
+              const MovieInfoEventWidget(),
+              ReusableContainer(
+                color: const Color(0xFFEAEAEA),
+                height: 20.w,
+                width: double.infinity,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      '29 Mars 2023',
+                      style: textBodyB,
+                    ),
+                  ),
+                ),
+              ),
+              const MovieInfoEventWidget(),
+              const MovieInfoEventWidget(),
+              const MovieInfoEventWidget(),
+              ReusableContainer(
+                color: const Color(0xFFEAEAEA),
+                height: 20.w,
+                width: double.infinity,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      '04 Avril 2023',
+                      style: textBodyB,
+                    ),
+                  ),
+                ),
+              ),
+              const MovieInfoEventWidget(),
+            ],
+          ),
+
+          const ChoiceCinemaWidget(),
+        ],
       ),
     );
   }
