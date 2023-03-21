ba8348b71b12597bec3be336c35d1b3fe9153b44
nStevenH <steven.huguenin@gmail.com>
2023-03-21 01:42:40 +0100

    Add weight for the project

---
+++ lib/src/constants/weight.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const FontWeight light = FontWeight.w300;
+const FontWeight regular = FontWeight.w400;
+const FontWeight medium = FontWeight.w500;
+const FontWeight bold = FontWeight.w700;
+const FontWeight extraBold = FontWeight.w900;
